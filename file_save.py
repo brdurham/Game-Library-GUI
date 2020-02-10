c197ced49650e8b3d7e510c1a02ddbb8672ef17e
brdurham <bdurham@stcharlessd.org>
2020-02-10 12:08:37 -0600

    new file:   file_save.py 	new file:   game_lib.pickle

---
+++ file_save.py
@@ -0,0 +1,14 @@
+#!usr/bin/python3
+#Brady Durham
+#1/28/20
+
+import pickle
+games = { 1: ['FPS', 'Halo3', 'Bungie', 'Microsoft', 'Xbox360', '2007',
+              '10', 'either', '$30.00', 'yes', '1/15/2008', 'This game blows chunks!'],
+          2: ['FPS', 'Doom', 'id Software', 'Bethesda Softworks', 'XboxOne', '2016',
+                  '10', 'either', '$10.00', 'no', '4/8/2018', 'Heavy metal music intensifies'] }
+
+
+datafile = open("game_lib.pickle", "wb")
+pickle.dump(games, data_file)
+datafile.close()
